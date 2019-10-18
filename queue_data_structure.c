533d4c2c228d9b8ae50f33e2195fedad31497317
Suleyman Suleymanzade <sul_soft@hotmail.com>
2019-10-18 14:24:47 +0400

    last queue update

--- queue_data_structure.c
+++ queue_data_structure.c
@@ -5,6 +5,7 @@ struct queue{
     int *buff;
     int buff_size;
     int first, last;
+    int count;
 
 };
 
@@ -14,23 +15,26 @@ struct queue* init_queue(int buff_size){
     q->buff = (int *) malloc(sizeof(int) * buff_size);
     q->last = q->first = 0;
     q->buff_size = buff_size;
+    q->count = 0;
     return q;
 }
 
 void enqueue(struct queue* q, int item){
-    if(q->last < q->buff_size){
+    if(q->count < (q->buff_size - 1)){
         q->buff[q->last] = item;
-        q->last = (q->last) + 1;
+        q->last = (q->last + 1) % q->buff_size;
+        q->count = (q->count) + 1;
     }    
 }
 
 int dequeue(struct queue* q){
     int res = -1e9;
 
-    if(q->last != q->first){
+    if(q->count > 0){
 
         res = q->buff[q->first];
-        q->first = (q->first) + 1;
+        q->first = (q->first + 1) % q->buff_size;
+        q->count = (q->count) - 1;
 
     }
     return res;
@@ -41,15 +45,20 @@ int dequeue(struct queue* q){
 int main(){
 
 
-    struct queue *q = init_queue(16);
+    struct queue *q = init_queue(5);
     enqueue(q, 23);
     enqueue(q, 55);
     enqueue(q, 77);
+    printf("%d\n", dequeue(q));
+    printf("%d\n", dequeue(q));
+    enqueue(q, 77);
     enqueue(q, 88);
 
-    while(q->last != q->first){
+    do{
         printf("%d\n", dequeue(q));
-    }
+    }while(q->last != q->first);
+
+
 
 
 return 0;
